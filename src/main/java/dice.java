12242425929a1e7c89e2716466358fca7c3c3d51
HDubuis <helene.dubuis@heig-vd.ch>
2020-04-20 16:53:30 +0200

    Add dice and test

--- src/main/java/dice.java
+++ src/main/java/dice.java
@@ -1,2 +1,38 @@
-public class dice {
+import java.util.Random;
+
+/**
+ * Class Dice
+ *
+ * Représente un dés à 6 faces
+ *
+ * @author Dubuis Hélène, Triponez Michaël
+ * @version 1.0
+ */
+class Dice {
+    // la face suppérieur du dés (pour voir la valeur obtenue)
+    protected int faceValue;
+
+    // Générateur de nombre aléatoire.
+    private final static Random rand = new Random(System.currentTimeMillis());
+
+    /**
+     * Constructeur
+     *
+     * On lance le dés à la création
+     */
+    public Dice() {
+        roll();
+    }
+    /**
+     * @return le résultat du lancé
+     */
+    public int getFaceValue() {
+        return faceValue;
+    }
+    /**
+     * Générer un nombre aléatoire entre 1 et 6, pour simuler le lancer de dés.
+     */
+    public void roll() {
+        faceValue = rand.nextInt(6) + 1;
+    }
 }
