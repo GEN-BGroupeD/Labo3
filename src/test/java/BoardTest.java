e7aca47ea6b041260aaed61aa4fa9e0f2c59d94b
HDubuis <helene.dubuis@heig-vd.ch>
2020-04-21 17:50:47 +0200

    A board with case

---
+++ src/test/java/BoardTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardTest {
+    @Test
+    void BoardTest(){
+        Board board = new Board();
+        assertEquals("Go",board.getSquare(0).toString());
+        for(int i= 1; i<40; i++){
+            assertEquals("Square "+i, board.getSquare(i).toString());
+        }
+
+    }
+
+}
