2d4504acf88b7babab7d76ac4d97ca19134e5946
HDubuis <helene.dubuis@heig-vd.ch>
2020-04-22 10:04:13 +0200

    Test player turn

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -4,10 +4,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
     @Test
+    void PlayerHaveCorrectName(){
+        Board board = new Board();
+        String testName = "Tintin";
+        Player player = new Player(testName,board);
+        assertEquals(testName, player.getName());
+    }
+    @Test
     void playerHavePieceAndStartAtGo() {
         Board board = new Board();
         Player player = new Player("Tintin", board);
         assertNotNull(player.getPiece());
         assertEquals("Go", player.getPiece().getLocation().toString());
+    }
+
+    @Test
+    void playerCanTakeTurn(){
+        Board board = new Board();
+        Dice dice = new Dice();
+        Player player = new Player("Tintin",board);
+        String oldCase = player.getPiece().getLocation().toString();
+        player.takeTurn(board,dice);
+        String newCase = player.getPiece().getLocation().toString();
+        assertNotEquals(oldCase,newCase);
     }
 }
