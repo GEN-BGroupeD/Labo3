44984d0a46cf907b753d7b344ea854275154e828
HDubuis <helene.dubuis@heig-vd.ch>
2020-04-22 09:49:42 +0200

    Piece + test

--- src/test/java/PieceTest.java
+++ src/test/java/PieceTest.java
@@ -1,5 +1,14 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PieceTest {
+    @Test
+    void pieceHaveNames(){
+        Piece piece = new Piece();
+        assertNotEquals(null,piece.getPieceName());
+    }
+
+
 
 }
